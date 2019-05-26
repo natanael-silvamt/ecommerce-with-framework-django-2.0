dbb3f51043fb3ad8562d17e76971b064fe6a78ca
Natanael <natanaelsilva.cc16@gmail.com>
2019-05-26 11:34:40 -0300

    criando checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     re_path(r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem,
         name='create_cartitem'
     ),
-    re_path(r'^carrinho/$', views.cart_item, name='cart_item')
+    re_path(r'^carrinho/$', views.cart_item, name='cart_item'),
+    re_path(r'^finalizando/$', views.checkout, name='checkout')
 ]
